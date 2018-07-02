6c4238bbd1a458e34153dadfb604bd407f274941
thtrung2006 <thtrung2006@gmail.com>
2018-07-02 13:52:36 +0700

    fix git ignore

--- Server/app.js
+++ Server/app.js
@@ -3,11 +3,30 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 
 var app = express();
 
-// view engine setup
+//Model implementation
+const
+  User  = require('./APIs/models/UserModel'),
+  Room = require('./APIs/models/RoomModel'),
+  Post = require('./APIs/models/PostModel'),
+  PostReport = require('./APIs/models/PostReportModel'),
+  Comment = require('./APIs/models/CommentModel')
+
+
+// view engine setup (for web admin)
+app.set('views', [path.join(__dirname, 'webadmin')])
+app.set('view engine', 'ejs')
+
+//Database connection
+try {
+  mongoose.connect("mongodb://localhost:27017/hosteldb")
+} catch (error) {
+  console.log(error)
+}
 
 
 app.use(logger('dev'));
@@ -15,6 +34,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+//Route Implementation
 
 
 // catch 404 and forward to error handler
