f8c99dc95a595053f4720655cd769c96b1d0436f
Trung Thai <thtrung2006@gmail.com>
2018-07-21 22:08:29 +0700

    edit profile

--- Server/WebAdminMVC/controllers/AdminController.js
+++ Server/WebAdminMVC/controllers/AdminController.js
@@ -2,29 +2,62 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
-exports.go_to_confirm_current_password = (req,res) => {
+exports.go_to_confirm_current_password = (req, res) => {
     res.render('old_password');
 }
 
-exports.confirm_current_password = async (req,res) => {
+exports.confirm_current_password = async (req, res) => {
     try {
         var currentPassword = (await User.findById(req.session.admin._id)).password;
-        if (currentPassword === req.body.password){
+        if (currentPassword === req.body.password) {
             res.render('new_password');
         } else {
-            res.render('old_password', {error : 'Your current password is wrong'});
+            res.render('old_password', { error: 'Your current password is wrong' });
         }
     } catch (error) {
         console.log(error);
-    }   
+    }
 }
 
-exports.create_new_password = async (req,res) => {
+exports.create_new_password = async (req, res) => {
     try {
-        await User.findByIdAndUpdate(req.session.admin._id, {"$set" : {
-            password : req.body.password
-        }});
+        await User.findByIdAndUpdate(req.session.admin._id, {
+            "$set": {
+                password: req.body.password
+            }
+        });
         res.redirect('/admin/logout');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.go_to_edit_profile = (req, res) => {
+    res.render('profile_edit');
+}
+
+exports.update_profile = async (req, res) => {
+    try {
+        var set;
+        if (req.file) {
+            set = {
+                name: req.body.name,
+                phone: req.body.phone,
+                image: '/images/users/' + req.file.filename
+
+            }
+        } else {
+            set = {
+                name: req.body.name,
+                phone: req.body.phone
+            }
+        }
+        var user_id = req.session.admin._id;
+        await User.findByIdAndUpdate(user_id, {"$set" : set});
+        var user = await User.findById(user_id);
+        delete user.password;
+        req.session.admin = user;
+        res.redirect('/admin/home');
     } catch (error) {
         console.log(error);
     }
--- Server/WebAdminMVC/routes/AdminRoute.js
+++ Server/WebAdminMVC/routes/AdminRoute.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const AdminController = require('../controllers/AdminController');
+const Multer = require('../../custom_modules/multer');
 
 module.exports = (app) => {
     app.route('/admin/change_password')
@@ -11,4 +12,10 @@ module.exports = (app) => {
 
     app.route('/admin/change_password/create_new_password')
         .post(AdminController.create_new_password);
+
+    app.route('/admin/edit_profile')
+        .get(AdminController.go_to_edit_profile);
+
+    app.route('/admin/edit_profile/update_profile')
+        .post(Multer.single('avatar'), AdminController.update_profile);
 }
